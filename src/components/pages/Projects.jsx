acdc1a300dd35cdc1bad7d56ea49b7b638e3ffb6
sandra <contato.sandracarvalho@gmail.com>
2022-10-31 23:07:41 -0300

    criando o dashboard com botão de Criar Projeto componentizado, iniciando a criação do dashboard completo

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,12 +1,31 @@
-import Message from "../layout/Message"
+import Message from "../layout/Message";
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton";
+
+import styles from "./Projects.module.css";
+
+import { useLocation } from "react-router-dom";
 
 function Projects() {
-    return(
-        <div>
-            <h1>Meus Projetos</h1>
-            <Message msg="alguma mensagem" type="success" />
-        </div>
-    )
+  const location = useLocation();
+  let message = "";
+
+  if (location.state) {
+    message = location.state.message;
+  }
+
+  return (
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton text="Criar projeto" to="/new-project" />
+      </div>
+      {message && <Message msg={message} type="success" />}
+      <Container customClass="start">
+    <p>Projetos...</p>
+      </Container>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
